b50502588ea4deeb28615e249f6c67228dc2feac
Cooper <davidontech@gmail.com>
2020-11-08 00:21:19 +1300

    Start to add effects

---
+++ effects/effects.py
@@ -0,0 +1,6 @@
+
+# cmd:
+# jalv -c Drive=2 http://guitarix.sourceforge.net/plugins/gx_amp#GUITARIX
+# jack_connect GxAmplifier-X:in system:capture_1
+# jack_connect GxAmplifier-X:out system:playback_1
+# jack_connect GxAmplifier-X:out system:playback_2
